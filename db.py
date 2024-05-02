c930aa386db05921fb5611bd9c315234bdcff80e
Christopher Cox <christophercox@MacBook-Pro-5.fios-router.home>
2024-05-01 22:58:15 -0400

    :page_facing_up: Updates to license

--- db.py
+++ db.py
@@ -1,3 +1,20 @@
+# Copyright (c) 2023-2024 Westfall Inc.
+#
+# This file is part of Windcarver.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, and can be found in the file NOTICE inside this
+# git repository.
+#
+# This program is distributed in the hope that it will be useful
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import sqlalchemy as db
 from sqlalchemy import inspect
@@ -48,12 +65,20 @@ if __name__ == '__main__':
 
     metadata = db.MetaData()
 
+    Model_Repo = db.Table('model_repo', metadata,
+              db.Column('id', db.Integer(), primary_key=True, unique=True),
+              db.Column('default_branch', db.String(255), nullable=False),
+              db.Column('full_name', db.String(), nullable=False),
+    )
+
     Commits = db.Table('commits', metadata,
               db.Column('id', db.Integer(), primary_key=True, unique=True),
               db.Column('ref', db.String(255), nullable=False), # branch
               db.Column('commit', db.String(255), nullable=False), # commit hash
+              db.Column('processed', db.Boolean(), nullable=False),
               db.Column('date', db.DateTime(), nullable=False) # commit date
     )
+
     Models = db.Table('models', metadata,
               db.Column('id', db.Integer(), primary_key=True, unique=True),
               db.Column("commit_id", db.Integer(), db.ForeignKey("commits.id"), nullable=False),
@@ -76,57 +101,63 @@ if __name__ == '__main__':
               db.Column('id', db.Integer(), primary_key=True, unique=True),
               db.Column("model_id", db.Integer(), db.ForeignKey("models.id"), nullable=False),
               db.Column("element_id", db.Integer(), db.ForeignKey("elements.id"), nullable=False),
+              db.Column("commit_id", db.Integer(), db.ForeignKey("commits.id"), nullable=False),
     )
     Requirements = db.Table('requirements', metadata,
               db.Column('id', db.Integer(), primary_key=True, unique=True),
               db.Column("commit_id", db.Integer(), db.ForeignKey("commits.id"), nullable=False),
               db.Column('declaredName', db.String(255), nullable=True),
               db.Column('shortName', db.String(255), nullable=True),
-              db.Column('qualifiedName', db.String(255), nullable=True), 
+              db.Column('qualifiedName', db.String(255), nullable=True),
               db.Column("element_id", db.Integer(), db.ForeignKey("elements.id"), nullable=False),
     )
+
     Verifications = db.Table('verifications', metadata,
               db.Column('id', db.Integer(), primary_key=True, unique=True),
               db.Column("commit_id", db.Integer(), db.ForeignKey("commits.id"), nullable=False),
               db.Column("element_id", db.Integer(), db.ForeignKey("elements.id"), nullable=False),
               db.Column("requirement_id", db.Integer(), db.ForeignKey("requirements.id"), nullable=False),
+              db.Column("verified", db.Boolean(), default=False),
+              db.Column("attempted", db.Boolean(), default=False)
     )
     Actions = db.Table('actions', metadata,
-              db.Column('id', db.Integer(), primary_key=True, unique=True),
-              db.Column("element_id", db.Integer(), db.ForeignKey("elements.id"), nullable=False),
-              db.Column("verifications_id", db.Integer(), db.ForeignKey("verifications.id"), nullable=False),
-              db.Column('declaredName', db.String(255), nullable=False),
-              db.Column('qualifiedName', db.String(), nullable=False),
-              db.Column('harbor', db.String(), nullable=True),
-              db.Column('artifacts', db.String(), nullable=True),
-              db.Column('variables', db.String(), nullable=True),
+        db.Column('id', db.Integer(), primary_key=True, unique=True),
+        db.Column("commit_id", db.Integer(), db.ForeignKey("commits.id"), nullable=False),
+        db.Column("element_id", db.Integer(), db.ForeignKey("elements.id"), nullable=False),
+        db.Column("verifications_id", db.Integer(), db.ForeignKey("verifications.id"), nullable=False),
+        db.Column('shortName', db.String(255), default=None),
+        db.Column('declaredName', db.String(255), nullable=False),
+        db.Column('qualifiedName', db.String(), nullable=False),
+        db.Column('harbor', db.String(), nullable=True),
+        db.Column('artifacts', db.String(), nullable=True),
+        db.Column('variables', db.String(), nullable=True),
+        db.Column('valid', db.Boolean(), default=False),
+        db.Column('dependency', db.Integer(), nullable=True, default=None)
     )
-    #Verifications_Actions = db.Table('verifications_actions', metadata,
-    #          db.Column('id', db.Integer(), primary_key=True),
-    #          db.Column("verifications_id", db.Integer(), db.ForeignKey("verifications.id"), nullable=False),
-    #          db.Column("actions_id", db.Integer(), db.ForeignKey("actions.id"), nullable=False),
-    #)
-
     Containers = db.Table('containers', metadata,
               db.Column('id', db.Integer(), primary_key=True, unique=True),
               db.Column("resource_url", db.String(255), nullable=False),
               db.Column("host", db.String(255), nullable=False),
               db.Column('project', db.String(255), nullable=False),
+              db.Column('project_id', db.Integer(), nullable=True),
               db.Column('image', db.String(255), nullable=False),
+              db.Column('image_id', db.Integer(), nullable=True),
               db.Column('tag', db.String(255), nullable=False),
     )
-
     Container_Commits = db.Table('container_commits', metadata,
               db.Column('id', db.Integer(), primary_key=True, unique=True),
               db.Column("containers_id", db.Integer(), db.ForeignKey("containers.id"), nullable=False),
               db.Column('digest', db.String(64), nullable=False),
               db.Column('date', db.DateTime(), nullable=False),
+              db.Column('cmd', db.String(), nullable=True),
+              db.Column('working_dir', db.String(255), nullable=True)
     )
 
     Artifacts = db.Table('artifacts', metadata,
               db.Column('id', db.Integer(), primary_key=True),
               db.Column('full_name', db.String(255), nullable=False), # artifact repo path
               db.Column('commit_url', db.String(), nullable=False), # artifact repo path
+              db.Column('default_branch', db.String(255), nullable=False),
     )
 
     Artifacts_Commits = db.Table('artifact_commits', metadata,
@@ -135,5 +166,18 @@ if __name__ == '__main__':
               db.Column('ref', db.String(255), nullable=False), # branch
               db.Column('commit', db.String(255), nullable=False), # commit hash
               db.Column('date', db.DateTime(), nullable=False) # commit date
+    )
+
+    Thread_Executions = db.Table('thread_executions', metadata,
+        db.Column('id', db.Integer(), primary_key=True, unique=True),
+        db.Column('name', db.String(255), nullable=False),
+        db.Column("action_id", db.Integer(), db.ForeignKey(Actions.c.id), nullable=False),
+        db.Column("model_commit_id", db.Integer(), db.ForeignKey(Commits.c.id), nullable=False),
+        db.Column("container_commit_id", db.Integer(), db.ForeignKey(Container_Commits.c.id), nullable=False),
+        db.Column("artifact_commit_id", db.Integer(), db.ForeignKey(Artifacts_Commits.c.id), nullable=False),
+        db.Column("source", db.String(255), nullable=False),
+        db.Column("state", db.String(255), nullable=False),
+        db.Column('date_created', db.DateTime(), nullable=False),
+        db.Column('date_updated', db.DateTime(), nullable=False),
     )
     metadata.create_all(engine)
